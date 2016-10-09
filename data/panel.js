d831023bad9105921893a51a3e76c9d720596e8a
S.D. Michaud <smich010@uottawa.ca>
2016-10-09 14:04:52 -0400

    Add Quick Change Buttons to Series and Episode
    Modify the entry table to contain buttons to quickly increment and
    decrement the series and episode counts. Utilizes a table within a table
    structure.

--- data/panel.js
+++ data/panel.js
@@ -8,17 +8,26 @@ $("#addButton").click(function(){
 // This could cause issues when it comes to buttons with text vs buttons without text (just an image)
 // TODO? refactor code so buttons use "classes" instead of text for identification
 $("#showTable").on("click", "tr button", function(){
-	var opSelected = $(this).text();
+	var buttonClassSelected = $(this).attr("class");
+	//console.log(buttonClassSelected)
 	let showName = $(this).parent().siblings(":first").text();
-	switch(opSelected){
-		case "Change":
+	// get the show name starting from the inner table, must break out of inner table first.
+	let quickShowName = $(this).closest("table").parent().siblings(":first").text();
+	switch(buttonClassSelected){
+		case "change":
 		    self.port.emit("change-button-click", showName);
 	        break;
-		case "Delete":
+		case "delete":
 		    let ok = window.confirm("Delete record for " + showName + "?");
 			if(ok){
 				self.port.emit("delete-button-click", showName);
 			}
+			break;
+		case "sPlus":
+		case "sMinus":
+		case "ePlus":
+		case "eMinus":
+		    self.port.emit("quick-button-click", {showName: quickShowName, buttonClass: buttonClassSelected});
 			break;
 		default:
 		    console.log("Default Switch: Error");
@@ -42,6 +51,40 @@ self.port.on("panel-show", function(savedEntries){
 // Flag checking if table has recently been updated with new data
 var isTableUpToDate = false;
 
+/* Structure: Table within table
+<table>
+  <tr>
+    <td>Show Name</td>
+	<td>
+	  <table>
+        <tr>
+          <td>
+	        <button>+</button>
+          </td>
+	      <td>
+	        <button>+</button>
+	      </td>
+        </tr>
+        <tr>
+          <td>S:1</td>
+	      <td>E:1</td>
+        </tr>
+        <tr>
+          <td>
+	        <button>-</button>
+	      </td>
+	      <td>
+	        <button>-</button>
+	      </td>
+        </tr>
+      </table>
+	</td>
+	<td>Option Buttons</td>
+  </tr>
+  
+</table>
+*/
+
 // Code that dynamically draws the table
 function CreateTable(savedEntries){
 	if(!isTableUpToDate){
@@ -50,10 +93,13 @@ function CreateTable(savedEntries){
 	    for (var ii = 0; ii < savedEntries.length; ii++){
 			// Create a row for this show and save a reference to it
 		    var tr = $("<tr/>").appendTo(tbody);
-			// This string holds the html for a table row
+			// This string holds the html for a table row, it is very complicated and I don't think there is any way around it
+			// There is a table within a table for the +/- buttons on the series and episode counts
 			var appendString = "<td>" + savedEntries[ii].name + "</td>" +
-			               "<td> S:" + savedEntries[ii].series + ", E:" + savedEntries[ii].episode + "</td>" +
-						   "<td><button>Change</button><br><button>Delete</button></td>";
+			               "<td>" + "<table><tr><td><button class='sPlus'>+</button></td><td><button class='ePlus'>+</button>" + "</td>" +
+			               "</tr><tr><td>" + "S: " + savedEntries[ii].series + ",</td><td> E: " + savedEntries[ii].episode + "</td>" +
+						   "</tr><tr><td>" + "<button class='sMinus'>-</button></td><td><button class='eMinus'>-</button>" + "</td></tr></table>" +
+						   "<td><button class='change'>Change</button><br><button class='delete'>Delete</button></td>";
 			tr.append(appendString);
 		}
 		
@@ -65,4 +111,8 @@ self.port.on("savedEntries-modified", function(){
 	// Reset html to default headers
 	$("#tableBody").html("<tr class='headings'><th>Series</th><th>Current Episode</th><th>Options</th>");
 	isTableUpToDate = false;
+});
+
+self.port.on("redraw-table", function(savedEntries){
+	CreateTable(savedEntries);
 });
